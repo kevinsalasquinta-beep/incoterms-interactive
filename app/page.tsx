69557ed2c9cce4d6924ec66a2410fbd922055005
kevinsalasquinta-beep <kevinsalasquinta@gmail.com>
2026-03-11 00:18:23 -0500

    Add files via upload
    Add app and components folders

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import IncotermsInteractive from "@/components/IncotermsInteractive";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <IncotermsInteractive />
+    </main>
+  );
+}
